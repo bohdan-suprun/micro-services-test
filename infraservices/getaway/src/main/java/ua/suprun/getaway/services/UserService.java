29ab6a737b19aafbda471a849863ec6a62d34cb1
Bohdan Suprun <bohdan_suprun@epam.com>
2019-03-08 14:51:47 +0200

    Initial turbine setup.

--- infraservices/getaway/src/main/java/ua/suprun/getaway/services/UserService.java
+++ infraservices/getaway/src/main/java/ua/suprun/getaway/services/UserService.java
@@ -1,7 +1,9 @@
 package ua.suprun.getaway.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ua.suprun.dto.user.UserDto;
@@ -17,6 +19,7 @@ public class UserService
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(defaultFallback = "userServiceIsNotAvailable")
     public String getUserRoleByEmail(String email) {
         final ResponseEntity<UserDto> entity = restTemplate
             .getForEntity("http://user/email/?userEmail={1}", UserDto.class, email);
@@ -24,5 +27,10 @@ public class UserService
         return entity.getBody() != null
             ? entity.getBody().getRole()
             : "";
+    }
+
+    private String userServiceIsNotAvailable()
+    {
+        throw new InternalAuthenticationServiceException("Can not retrieve user credentials");
     }
 }
--- infraservices/turbine/src/main/java/ua/suprun/turbine/TurbineApp.java
+++ infraservices/turbine/src/main/java/ua/suprun/turbine/TurbineApp.java
@@ -1,25 +1,24 @@
-package ua.suprun.getaway;
+package ua.suprun.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
- * Class GetawayApp implementation.
+ * Class TurbineApp implementation.
  *
  * @author Bohdan_Suprun
  */
 @SpringBootApplication
-@EnableZuulProxy
 @EnableEurekaClient
-@EnableCircuitBreaker
-public class GetawayApp
+@EnableHystrixDashboard
+@EnableTurbine
+public class TurbineApp
 {
-
     public static void main(String[] args)
     {
-        SpringApplication.run(GetawayApp.class, args);
+        SpringApplication.run(TurbineApp.class, args);
     }
 }
